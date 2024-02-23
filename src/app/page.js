b67ba97409a72ad9e9e16bd58da58a3c342356f1
Shivam Jaiswal <76699472+shivam-jaiswal-2002@users.noreply.github.com>
2024-02-23 20:53:06 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,46 +5,53 @@ import Skill from "@/components/Skill/page";
 import Image from "next/image";
 import SkillName from "@/components/Skill/SkillName/page";
 import { Link, animateScroll as scroll } from "react-scroll";
-import Fade from 'react-reveal/Fade';
-import VisibilitySensor from 'react-visibility-sensor';
+import { Fade } from "react-awesome-reveal";
+import VisibilitySensor from "react-visibility-sensor";
 import Projects from "@/components/Projects/page";
 export default function Home() {
   return (
-    
-    <main id="myself" className="font-serif bg flex min-h-screen flex-col m-7 justify-between p-24">
+    <main
+      id="myself"
+      className="font-serif bg flex min-h-screen flex-col m-7 justify-between p-24"
+    >
       {/* Myself Page */}
-      <div  className="flex">
+      <div className="flex">
         <div className="w-1/2">
-        <MeL/>
-
+          <MeL />
         </div>
         <div className="w-1/2 my-auto">
-        <MeR/>
-          
+          <MeR />
         </div>
       </div>
 
-
-
-      {/* Skills Page */}
+      {/* Skills and SkillName Pages */}
       <div id="skills" className="flex mt-40 pt-40">
-  <VisibilitySensor partialVisibility>
-    {({ isVisible }) =>
-      <Fade bottom when={isVisible}>
-        <div className="w-1/2 item-center">
-          <Skill/>
+        <div className="w-1/2 m-auto">
+          <VisibilitySensor partialVisibility>
+            {({ isVisible }) =>
+              <Fade bottom when={isVisible}>
+                <Skill/>
+              </Fade>
+            }
+          </VisibilitySensor>
         </div>
-        <div className="w-1/2 flex my-auto justify-end">
-          <SkillName/>
+        
+        <div className="w-1/2">
+          <VisibilitySensor partialVisibility>
+            {({ isVisible }) =>
+              <Fade bottom when={isVisible}>
+                <div className="flex justify-end">
+                  <SkillName/>
+                </div>
+              </Fade>
+            }
+          </VisibilitySensor>
         </div>
-      </Fade>
-    }
-  </VisibilitySensor>
-</div>
+      </div>
 
-<div className="mt-40 m-auto pt-40">
-  <Projects/>
-</div>
+      <div className="mt-40 m-auto pt-40">
+        <Projects />
+      </div>
     </main>
   );
 }
