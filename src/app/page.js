5e842132a095af847a5745100b08c7732f389e11
shivam-jaiswal-20020225 <shivam.jaiswal2020@vitstudent.ac.in>
2024-02-26 15:10:57 +0530

    updated certificate page

--- src/app/page.js
+++ src/app/page.js
@@ -65,7 +65,7 @@ export default function Home() {
         <hr />
         <Projects />
       </div>
-
+      
       <div>
         <Certifications />
       </div>
--- src/components/CertificateBox/page.jsx
+++ src/components/CertificateBox/page.jsx
@@ -3,21 +3,22 @@ import { Fade } from "react-awesome-reveal";
 import VisibilitySensor from "react-visibility-sensor";
 const CertificateBox = ({ name, image }) => {
   return (
+    <VisibilitySensor partialVisibility>
+    {({ isVisible }) => (
+      <Fade bottom when={isVisible}>
     <div className="bg-gray-400 hover:bg-gray-200 active:bg focus:outline-none focus:ring focus:ring-violet-300 shadow-lg rounded-lg p-4 flex sm:flex-col items-center justify-center">
-              <VisibilitySensor partialVisibility>
-          {({ isVisible }) => (
-            <Fade bottom when={isVisible}>
-              <div>
-              <div className="mb-4">
-        <Image src={image} alt={name} layout="responsive" width={80} height={80} />
+
+<div>
+      <div className="mb-4">
+<Image src={image} alt={name} layout="responsive" width={80} height={80} />
+</div>
+<p className="text-lg font-bold text-black text-center">{name}</p>
       </div>
-      <p className="text-lg font-bold text-black text-center">{name}</p>
-              </div>
-            </Fade>
-          )}
-        </VisibilitySensor>
-  
-    </div>
+</div>
+      </Fade>
+    )}
+  </VisibilitySensor>
+
   );
 };
 
