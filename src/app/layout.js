79de14b1ccdf5fa9b014091944c5274524c41cc6
shivam-jaiswal-2002 <shiv2002am@gmail.com>
2024-03-14 11:37:39 +0530

    added analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar/page";
 import Contact from "@/components/Contact/page";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({ children }) {
         <Navbar />
         {children}
       <Contact />
+      <Analytics/>
 
       </body> 
     </html>
